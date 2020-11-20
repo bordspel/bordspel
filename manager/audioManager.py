b9c2513b9e9420398ba266f992c28bac5135e822
thomaspj10 <thomaspj2003@gmail.com>
2020-11-20 22:07:36 +0100

    Update audioManager.py

--- manager/audioManager.py
+++ manager/audioManager.py
@@ -10,8 +10,14 @@ class AudioManager:
     def loadAudio(self, path):
         soundFile = self.minim.loadFile(path)
         self.audioFiles[path] = soundFile
+
+    def isAudio(self, path):
+        return path in self.audioFiles
     
     def getAudio(self, path):
         if path in self.audioFiles:
             return self.audioFiles[path]
         return False
+
+    def getAudioCount(self):
+        return size(self.audioFiles)
