0347a64b2dbfa61f0289a90ebd9f7895d782511e
Djoowel <74959812+Djoowel@users.noreply.github.com>
2021-01-13 13:44:03 +0100

    steekspel updated

--- minigames/steekspel/steekspel.py
+++ minigames/steekspel/steekspel.py
@@ -14,7 +14,7 @@ y_speler2 = screenHeight / 2
 velocity1 = 10
 velocity2 = -10 
 hartjeX = random.randint(100, screenWidth - 100)
-hartjeY = screenHeight * 0.71
+hartjeY = screenHeight * 0.55
 hartjeSpeed = 50
 meterDelay = 0
 winnerDelay = 0
@@ -46,20 +46,15 @@ def drawMeter(layer, element):
     fill(200, 0, 0)
     rect(buttonX , buttonY, 250, 180)
     #Meter
-    fill(254, 127, 156)
-    rect(0, screenHeight * 0.75, screenWidth, 300)
-    #Verticale streep
-    fill(0)
-    strokeWeight(4)
-    line(screenWidth / 2, screenHeight, screenWidth / 2, screenHeight * 0.75)
-    strokeWeight(1)
-    #Cirkel
-    fill(200, 0, 0)
-    circle(hartjeX, hartjeY, 185)
+    imageMeter = loadImage('./Meter.png')
+    image(imageMeter, 0, screenHeight * 0.85)
+    #Hartje
+    imageMeter = loadImage('./Hart.png')
+    image(imageMeter, hartjeX, hartjeY)
     #Geeft snelheid aan de cirkel
     hartjeX = hartjeX + hartjeSpeed
     #Check of de cirkel aan eind of aan het begin is, zo ja dan gaat die de andere kant op
-    if hartjeX > screenWidth - 100 or hartjeX < 100:
+    if hartjeX > screenWidth - 50 or hartjeX < -100:
         hartjeSpeed = hartjeSpeed * -1
     #Checkt of je op de knop drukt, zodat het hartje stop en vanuit daar krijg je een score en ga je naar de volgende room
     if mousePressed == True:
@@ -73,37 +68,55 @@ def drawMeter(layer, element):
                 score = 3
             else:
                 score = 4
-    if score > 0:
+    if score > 0 and meterDelay != -1:
         meterDelay += 1
         if meterDelay >= 130:
+            meterDelay == -1
             # =================================
             # = CODE VAN NETWORKING!          =
             # = Stuur de score naar de server =
             # =================================
+            print('ggggg')
             gameManager.client.send("steekspel", {"score": score})
-
-            # DIT MOET NAAR DE FUNCTIE `networkListener()` toe!!!!
             layer.removeElement(elementMeter)
+            elementLoading.registerDrawListener(drawLoadingScreen)
             elementMeter.unregisterDrawListener(drawMeter)
             elementSpeler1.registerDrawListener(drawSpeler)
             elementSpeler2.registerDrawListener(drawSpeler)
         
 def drawSpeler(layer, element):
-    if element.id == 'Speler1':
-        background(254, 127, 156)
-    global velocity1, velocity2, winnerDelay
-    fill(0,0,128)
+    if element.id == 'Speler1' or elementSpeler1.hidden:
+        # background(254, 127, 156)
+        background(79, 195, 247)
+    global velocity1, velocity2, winnerDelay, winner, gelijkspel 
     #Tekent 2 spelers
-    rect(element.x, element.y, spelerWidth, spelerHeight)
+    if element.id == 'Speler1':
+        imageHorseRight = loadImage('./HorseRight.png')
+        image(imageHorseRight, element.x, element.y)
+    if element.id == 'Speler2':
+        imageHorseLeft = loadImage('./HorseLeft.png')
+        image(imageHorseLeft, element.x, element.y)
+    fill(109, 76, 65)
+    rect(0, screenHeight, screenWidth, screenHeight * 0,80)
     if element.id == "Speler1":
         element.x += velocity1 
     if element.id == "Speler2":
         element.x += velocity2
     #Colision check
     if elementSpeler2.x - elementSpeler1.x - spelerWidth < 10:
-        elementSpeler2.unregisterDrawListener(drawSpeler)
-        winnerDelay += 1
-        velocity1 *= 0.95
+        if gelijkspel:
+            winnerDelay += 1
+            velocity1 *= 0.95
+            velocity2 *= 0.95
+        else:
+            if winner:
+                elementSpeler2.unregisterDrawListener(drawSpeler)
+                winnerDelay += 1
+                velocity1 *= 0.95
+            else:
+                elementSpeler1.hide()
+                winnerDelay += 1
+                velocity2 *= 0.95
         if winnerDelay >= 180:
             elementEind.registerDrawListener(drawEind)
             elementSpeler1.unregisterDrawListener(drawSpeler)
@@ -146,15 +159,32 @@ def resetSteekspel():
 def drawBackground(layer, element):
     background(254, 127, 156)
 
+def drawLoadingScreen(layer, element):
+    elementBackground.registerDrawListener(drawBackground)
+    layer.addElement(textLoading)
+
 # Dit bepaald wie de winnaar is.
 def networkListener(client, data):
+    global gelijkspel, winner
+    print(data)
     if data["type"] == "steekspel":
         winner = gameManager.client.id == data["winner"]
         gelijkspel = data["winner"] == "NONE"
-
-        # HIER MOET DE CODE VAN DE WIN/LOSE ANIMATIE KOMEN
         print(winner)
-        print(gelijkspel)
+        # HIER MOET DE CODE VAN DE WIN/LOSE ANIMATIE KOMEN
+        #if gelijkspel:
+        #     textEind.setText("Het is gelijkspel")
+        # else:
+        #     if winner:
+        #         textEind.setText("Jij bent de winnaar")
+        #     else:
+        #         textEind.setText("Jij bent de verliezer")    
+        print('peop')
+        elementLoading.unregisterDrawListener(drawLoadingScreen)
+        elementBackground.unregisterDrawListener(drawBackground)
+        layer.removeElement(textLoading)
+        elementSpeler1.registerDrawListener(drawSpeler)
+        elementSpeler2.registerDrawListener(drawSpeler)
 
 gameManager.client.register_listener(networkListener)
 
@@ -163,20 +193,21 @@ elementSpeler1 = layer.createElement("Speler1", x_speler1, y_speler1)
 elementSpeler2 = layer.createElement("Speler2", x_speler2, y_speler2)
 
 elementSpatie = layer.createElement("Spatie", 0, 0)
-# elementSpatie.registerKeyListener(keyIntro)
 
 elementBackground = layer.createElement('Background', 0, 0)
 
-elementMeter = layer.createElement("Meter", 200, 200)
+elementLoading = layer.createElement('LoadingScreen', 0, 0)
 
-# layer.addElement(textIntro)
+elementMeter = layer.createElement("Meter", 200, 200)
 
 elementEind = layer.createElement("Eind", 0, 0)
 
 textIntro = Text("TextIntro", screenWidth / 2, screenHeight / 2, "Welkom bij het Steekspel!\n\nIn deze minigame ga je tegen elkaar strijden door middel van liefde.\nU krijgt zo een liefde meter te zien met een hartje die snel heen en weer beweegt.\nDe bedoeling is om dat hartje zo dicht mogelijk in het midden te krijgen van de liefde meter.\nHoe dichter het hartje in het midden is, hoe meer liefde je heb en hoe groter de kans is dat je wint.\nOm het hartje stop te zetten drukt u met uw muisknop op de rode knop rechtsboven.\n\n\n\n\nDruk op spatie om verder te gaan.")
 textIntro.setTextSize(26)
+textIntro.setColor(255)
 
-textEind = Text("TextEind", screenWidth / 2, screenHeight / 2, "De winnaar is speler 1!\n\n\nAls beloning krijgt de winnaar: ... Charisma en ... Dukaten")
-textEind.setTextSize(30)
+textLoading = Text("TextLoading", screenWidth / 2, screenHeight / 2, "Aan het wachten op de andere speler...")
+textLoading.setTextSize(34)
+textLoading.setColor(255)
 
 startSteekspel()
